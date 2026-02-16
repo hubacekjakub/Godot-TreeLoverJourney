29af864dcec049d25e55f17e1305e37ec861471c
Jakub Hubacek <hubacek.jakub@gmail.com>
2026-02-17 00:59:43 +0100

    Small fixes

--- scripts/main_level.gd
+++ scripts/main_level.gd
@@ -3,7 +3,8 @@ class_name MainLevel
 
 @export var resting_places: Array[RestingPlace]
 
-@onready var nositka: Stretcher = $Nositka
+@onready var nositka: Stretcher = $Stretcher
 
 func _enter_tree() -> void:
 	Global.set_resting_places(resting_places)
+	
