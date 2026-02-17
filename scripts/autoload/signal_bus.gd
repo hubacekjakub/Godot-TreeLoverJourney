a471edeb068df2780ef26f7b61be79dad612f0eb
Jakub Hubacek <hubacek.jakub@gmail.com>
2026-02-17 08:27:03 +0100

    Fixed uid conflict

--- scripts/autoload/signal_bus.gd
+++ scripts/autoload/signal_bus.gd
@@ -2,10 +2,21 @@ extends Node
 
 
 # Map events
+
+# called when stretcher reached new resting place
+@warning_ignore("unused_signal")
 signal resting_place_reached()
+
+# called when new location for resting place is set
+@warning_ignore("unused_signal")
 signal new_resting_place_set(new_resting_place: RestingPlace)
 
+# called when new map is loaded
+@warning_ignore("unused_signal")
 signal map_loaded(map_name: String)
 
 #UnitDirector
+
+# called when new unit is selected
+@warning_ignore("unused_signal")
 signal on_unit_selected(unit: CollectorUnit)
--- scripts/test/nav_level.gd
+++ scripts/test/nav_level.gd
@@ -2,7 +2,7 @@ extends Node3D
 
 @onready var point_a: Node3D = $PointA
 @onready var point_b: Node3D = $PointB
-@onready var character_body_3d: NavAgent = $CharacterBody3D
+@onready var character_body_3d: CollectorUnit = $CharacterBody3D
 @onready var camera_3d: Camera3D = $Camera3D
 
 @export var RAY_LENGTH = 1000.0
